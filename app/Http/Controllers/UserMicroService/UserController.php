605f6013e1e6e3646b1540bd221bf5b59514a50d
Emeka Augustine <cafafanscoders@gamil.com>
2019-05-13 21:59:14 -0400

    Implemented Restful CRUD on Controller

--- app/Http/Controllers/UserMicroService/UserController.php
+++ app/Http/Controllers/UserMicroService/UserController.php
@@ -2,18 +2,79 @@
 
 namespace App\Http\Controllers\UserMicroService;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use App\Services\UserService;
 
 class UserController extends Controller
 {
+
+    use ApiResponser;
+
+    /**
+     * The service to consume the users micro service
+     * @var string
+     */
+    public $userService;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        //
+    public function __construct(UserService $userService) {
+        $this->userService = $userService;
     }
 
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return $this->successResponse($this->userService->getUsers());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        return $this->successResponse($this->userService->createUser($request->all()), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show($user) {
+        return $this->successResponse($this->userService->getUser($user));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $user) {
+        return $this->successResponse($this->userService->editUser($request->all(), $user), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($user) {
+        return $this->successResponse($this->userService->deleteUser($user));
+    }
+
 }
